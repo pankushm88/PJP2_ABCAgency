9fb1396507dcaebd0b02da2600554f8b359de88f
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:05:42 +0530

    ABC_Agency Assignment done successfully

--- src/main/java/com/sapient/PojoClass.java
+++ src/main/java/com/sapient/PojoClass.java
@@ -8,5 +8,38 @@ package com.sapient;
  *
  */
 public class PojoClass {
+	String city,country,gender,TypeOfCurrency;
+	double amount;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getTypeOfCurrency() {
+		return TypeOfCurrency;
+	}
+	public void setTypeOfCurrency(String typeOfCurrency) {
+		TypeOfCurrency = typeOfCurrency;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 
 }
