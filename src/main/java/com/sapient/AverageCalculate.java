fb9279b5805113d2479afa16a387d6b3559c0a33
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:00:15 +0530

    ABC_Agency Assignment done successfully

--- src/main/java/com/sapient/AverageCalculate.java
+++ src/main/java/com/sapient/AverageCalculate.java
@@ -1,5 +1,7 @@
 /**
  * 
+ *//**
+ * 
  */
 package com.sapient;
 
@@ -7,6 +9,30 @@ package com.sapient;
  * @author Pankush
  *
  */
-public class AverageCalculate {
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String inputFile = "input.csv";
+		String outputFile = "output.csv";
+		
+		IncomeCalcuate Ic = new IncomeCalcuate();
+		
+		try {
+			
+			Ic.ReadFile(inputFile);
+			List<String> MyList=AverageCalculate.average(Ic.map);
+			Collections.sort(MyList);
+			Ic.WriteFile(MyList,outputFile);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+}
+    }
 }
