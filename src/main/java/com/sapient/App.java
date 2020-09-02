7308abcf887b0b2184bed44d800fb48c77a08c6e
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:01:47 +0530

    ABC_Agency Assignment done successfully

--- src/main/java/com/sapient/App.java
+++ src/main/java/com/sapient/App.java
@@ -7,6 +7,30 @@ package com.sapient;
  * @author Pankush
  *
  */
-public class App {
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String inputFile = "input.csv";
+		String outputFile = "output.csv";
+		
+		IncomeCalcuate Ic = new IncomeCalcuate();
+		
+		try {
+			
+			Ic.ReadFile(inputFile);
+			List<String> MyList=AverageCalculate.average(Ic.map);
+			Collections.sort(MyList);
+			Ic.WriteFile(MyList,outputFile);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+}
+    }
 }
