bf95a6fd016ac6c6853132ce983e8d5843837c8d
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:04:56 +0530

    ABC_Agency Assignment done successfully

--- src/main/java/com/sapient/MyClass.java
+++ src/main/java/com/sapient/MyClass.java
@@ -9,4 +9,24 @@ package com.sapient;
  */
 public class MyClass {
 
+	Double amount;
+	int people;
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public int getPeople() {
+		return people;
+	}
+	public void setPeople(int people) {
+		this.people = people;
+	}
+	public MyClass(Double amount, int people) {
+		super();
+		this.amount = amount;
+		this.people = people;
+	}
 }
+
